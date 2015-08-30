f6fa9614c53f2ebc2bdf0ee01a407cf62f8d9a52
Lukas Boersma <lukas.boersma@rwth-aachen.de>
2015-08-31 00:04:29 +0200

    Version bump to v1.0.1

--- MarkdownDocNet/Properties/AssemblyInfo.cs
+++ MarkdownDocNet/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MarkdownDocNet")]
-[assembly: AssemblyDescription("A Markdown documentation generator for .NET XML documentation files.")]
+[assembly: AssemblyDescription("A Markdown documentation generator for .NET XML documentation files. More information at https://github.com/LukasBoersma/MarkdownDocNet")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Lukas Boersma")]
 [assembly: AssemblyProduct("MarkdownDocNet")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
